b62347f501d28bfcf9033f345737b5618dd0e54a
Nestor <nestor.venegas95@gmail.com>
2021-04-07 14:56:44 -0500

    commit tree 25290e7041a5d67a972dac568b2ca1cfb3eb4f50

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
+document.addEventListener("DOMContentLoaded", function(){
+    fetchDogImages()
+})
+
 console.log('%c HI', 'color: firebrick')
+
+
+function fetchDogImages(){
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    return fetch(imgUrl)
+    .then (resp => resp.json()) 
+    .then (dogImages => {
+    dogImages.message.forEach(dogImage => console.log(dogImage))   
+    });
+}
+
+function renderDogImage(dogImage){
+    const dogImageContainer = document.querySelector('#dog-image-container');
+    const img = document.createElement('img');
+    img.rc = dogImage;
+    dogImageContainer.append(img);
+} 
+
+
+
+// function renderBooks(books) {
+//     const main = document.querySelector('main');
+//     books.forEach(book => {
+//       const h2 = document.createElement('h2');
+//       h2.innerHTML = book.name;
+//       main.appendChild(h2);
+//     });
+
